69bd33732aecd6a83def7fd0d642c21f9c71cc31
Rodrigo <rodrigo@asmalljob.com>
2024-11-03 16:13:53 +0100

    deploy setup

--- index.mjs
+++ index.mjs
@@ -79,6 +79,7 @@ app.use((req, res) => {
 });
 
 // desplegar el servidor
-app.listen(3001, () => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
 	console.log('Server is running on port 3001');
 });
